cc74944a8a7e417ff5cecbb75aba1884798a2515
leandroluz97 <leandrodaluz97@gmail.com>
2022-01-30 23:51:02 +0000

    calendar design system styles

--- src/components/DueDate/index.tsx
+++ src/components/DueDate/index.tsx
@@ -6,6 +6,7 @@ import DatePicker from "react-datepicker";
 interface DueTimeProps {
   dueDate: string;
 }
+
 const DueDate = ({ dueDate }: DueTimeProps) => {
   const { selectedTask, updateTask } = useData();
 
@@ -14,35 +15,49 @@ const DueDate = ({ dueDate }: DueTimeProps) => {
   async function handleSaveDateTimeOnBlur() {
     if (!date) return;
 
-    await updateTask({
-      id: selectedTask.id,
-      name: selectedTask.name,
-      dueTime: date as unknown as string,
-      description: selectedTask.description,
-      listId: selectedTask.listId,
-      position: selectedTask.position,
-      tags: selectedTask.tags,
-    });
+    // await updateTask({
+    //   id: selectedTask.id,
+    //   name: selectedTask.name,
+    //   dueTime: date as unknown as string,
+    //   description: selectedTask.description,
+    //   listId: selectedTask.listId,
+    //   position: selectedTask.position,
+    //   tags: selectedTask.tags,
+    // });
   }
 
+  console.log(date);
+
+  function handleChange(d: any) {
+    setDate(d);
+  }
+
+  const minTime = new Date().getHours() as unknown as Date;
+  const maxTime = new Date().getHours() as unknown as Date;
+
   return (
-    <div>
+    <div className={styles.date}>
       <h3>Due Date</h3>
       <DatePicker
         selected={date}
-        onChange={(d) => setDate(date)}
-        locale="en-US"
+        value={date}
+        onChange={handleChange}
+        locale="pt-PT"
+        // dateFormat="dd/MM/yyyy h:mm aa"
         showTimeSelect
         className="red-border"
         timeFormat="p"
-        timeIntervals={15}
+        timeIntervals={5}
         dateFormat="Pp"
+        onBlur={handleSaveDateTimeOnBlur}
+        // isClearable
+        minDate={new Date()}
+        minTime={minTime}
+        maxTime={maxTime}
       />
 
       {/* <input type="datetime-local" name="" id="" onChange={(e) => setDate(e.target.value)} value={dates} onBlur={handleSaveDateTimeOnBlur} /> */}
-      {/*dates.length > 0 ? (
-        <button onClick={() => setDate("")}>Remove</button>
-      ) : null*/}
+      {/* {date ? <button onClick={() => setDate(undefined)}>Remove</button> : ""} */}
     </div>
   );
 };
